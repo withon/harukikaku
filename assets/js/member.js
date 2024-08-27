4147f98015d4a7ac3fe444d7c05e6b9ae654207d
withon <withon.wei@gmail.com>
2024-08-27 21:19:58 +0800

    feat:新增团队成员页面

--- assets/js/member.js
+++ assets/js/member.js
@@ -1,14 +1,14 @@
 /****************************************************
- * 首页
+ * 团队成员
  ***************************************************/
 /**
  * 聚焦栏和视频栏，首次加载时及页面大小调整时，更改样式
  */
 $(window).on("load resize", function () {
-  // B站外链的高度设为宽度的0.7倍
-  if ($("#bilibiliIframe").length) {
-    $("#bilibiliIframe").css({
-      height: $("#bilibiliIframe").width() * 0.7,
+  // 保持聚焦栏的大小与页面相同，使背景图片大小填充70%屏幕
+  if ($(".spotlight").length) {
+    $(".spotlight").css({
+      height: Math.max($(window).width() * 0.3, 220) + "px",
     });
   }
 });
